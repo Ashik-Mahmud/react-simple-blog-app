0051da82d832d0570420b3f9196febe032771b19
ashikmahmud <ashikmamud187@gmail.com>
2022-04-06 00:17:15 +0600

    added new section called videos

--- src/components/Videos/Videos.jsx
+++ src/components/Videos/Videos.jsx
@@ -1,13 +1,44 @@
 import React from "react";
+import styled from "styled-components";
 
 const Videos = () => {
   return (
     <div>
       <div className="container">
-        <h1>Videos</h1>
+        <VideoContainer>
+          <div className="title">
+            <h1>
+              Introduce my <span className="colorize">Self</span>
+            </h1>
+            <p>here is my introduce videos</p>
+          </div>
+          <div className="videos">
+            <iframe
+              src="https://www.youtube.com/embed/abep0mVSSys"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </VideoContainer>
       </div>
     </div>
   );
 };
 
+const VideoContainer = styled.div`
+  text-align: center;
+  padding: 4rem 0rem;
+  position: relative;
+  .title {
+    padding: 1rem 0rem;
+  }
+  iframe {
+    width: 800px;
+    aspect-ratio: 16/9;
+    border: 10px solid var(--primary-color);
+    border-radius: 5px;
+  }
+`;
 export default Videos;
